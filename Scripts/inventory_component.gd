865b929ff1ae443624040dac859c17b68c05259b
callumstreets21 <149797713+callumstreets21@users.noreply.github.com>
2024-10-12 18:46:02 +0100

    added WIP inventory and interaction system

---
+++ Scripts/inventory_component.gd
@@ -0,0 +1,45 @@
+extends Node
+##This inventory system works with two major categories of items
+##
+##	keys and holdable items:
+##
+##		keys are simple, they are a logical items that flick a bool in the collectable manager
+##	
+##		holdable items, there are two categories - car parts and tools:
+##			tools: one handed items, flashlight is useable while holding
+##			car parts: two handed, the flashlight isnt usable while holding
+##		
+##		you can only hold one holdable item at once
+
+func pick_up_car_part(item_to_pickup: RigidBody3D) -> bool:
+	##THIS FUNCTION IS EXPLICITLY FOR CAR PARTS
+	##USE pick_up_key() for keys
+	##USE pick_up_tool() for tools
+	
+	#check if there is already an item held and if the item attempting to pickup is in correct group
+	if get_child_count() == 0 and item_to_pickup.is_in_group("car_parts"):
+		item_to_pickup.reparent($ItemSlotTransform, false)
+		return true
+		
+	return false
+
+func pick_up_tool(item_to_pickup: RigidBody3D) -> bool:
+	##THIS FUNCTION IS EXPLICITLY FOR TOOLS
+	##USE pick_up_key() for keys
+	##USE pick_up_car_part() for car parts
+	
+	#check if there is already an item held and if the item attempting to pickup is in correct group
+	if get_child_count() == 0 and item_to_pickup.is_in_group("tool"):
+		item_to_pickup.reparent($ItemSlotTransform, false)
+		return true
+		
+	return false
+
+func pick_up_key() -> void:
+	##THIS FUNCTION IS EXPLICITLY FOR TOOLS
+	##USE pick_up_tool() for tools
+	##USE pick_up_car_part() for car parts
+	pass
+
+func throw_held_item(throwing_power: float) -> void:
+	pass
---
+++ Scripts/player_interaction_component.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func InteractWithItem() -> bool:
+## item interaction function.
+## returns true if interaction successful
+## returns false if interaction failed 
+	if not $RayCast3D.is_colliding():
+		return false
+	else:
+		var target = $RayCast3D.get_collider()
+		return true
+		
+		
