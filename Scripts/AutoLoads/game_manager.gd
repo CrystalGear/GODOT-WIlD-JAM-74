619807e5190633f0ff8f476754a0c89ffc2a84cf
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-10-13 20:52:57 +0100

    stop pause showing on menus

--- Scripts/AutoLoads/game_manager.gd
+++ Scripts/AutoLoads/game_manager.gd
@@ -3,6 +3,7 @@ extends Node
 # Constants
 var PAUSE_TIME: float = 3.0
 var PAUSE_UI:PackedScene = preload("res://Scenes/pause.tscn")
+var DISALLOWED_PAUSE_LEVELS = ["SplashScreen", "MainMenu", "OptionsMenu", "WinScreen", "CreditsMenu", "GameOverScreen"]
 
 # Runtime variables
 var b_paused:bool = false
@@ -15,12 +16,21 @@ func _ready() -> void:
 	add_child(pause_ui)  			# Add it to the current scene
 	pause_ui.hide()  				# Make sure it starts hidden
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	_player_input()
 	
 func _player_input() -> void:
+	
+	# Exit early if the scene is null (liek when transitioning)
+	if get_tree().current_scene == null:
+		return	
+		
+	# Get the current scene	
+	var current_scene = get_tree().current_scene.name
+	if current_scene in DISALLOWED_PAUSE_LEVELS:
+		return
+		
 	if(Input.is_action_just_pressed("pause")):
 		if(b_paused):
 			pause_ui.hide()
@@ -36,7 +46,6 @@ func _fake_pause() -> void:
 	# Set paused to true
 	_toggle_pause(true)
 	
-	
 	# Start a timer and store it.
 	pause_timer = Timer.new()
 	add_child(pause_timer)
@@ -45,7 +54,6 @@ func _fake_pause() -> void:
 	pause_timer.start()
 	
 	pause_timer.connect("timeout", Callable(self, "_unpause"))
-	
 	
 func _unpause() -> void:
 	# Stop the pause timer only if it's still active (not null)
