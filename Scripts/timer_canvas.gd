2d14bc090ba1f61983e0e3073dccb297d7d8c905
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2024-10-16 17:38:29 +0100

    Added countdown timer that prints to  a text box

---
+++ Scripts/timer_canvas.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var timer_text: Label = $timer_text
+
+
+@export var timer: float = 120.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer -= delta
+	timer_text.text = str(timer)
