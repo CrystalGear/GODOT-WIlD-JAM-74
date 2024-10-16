4165b814d57bf7421d4480cdc994a9786f9938f3
callumstreets21 <149797713+callumstreets21@users.noreply.github.com>
2024-10-16 12:24:34 +0100

    pick up function added to interaction component

--- Scripts/player_interaction_component.gd
+++ Scripts/player_interaction_component.gd
@@ -12,3 +12,15 @@ func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group("interactable"):
 		item.set_outline_visibility(false)
 		item = null
+
+func pick_up_item() -> void:
+	if item:
+		if item.is_class("Car_part"):
+			$InventoryComponent.pick_up_car_part(item)
+			return
+		elif item.is_class("Tool"):
+			$InventoryComponent.pick_up_tool(item)
+			return
+		elif item.is_class("Key"):
+			$InventoryComponent.pick_up_key()
+			return
