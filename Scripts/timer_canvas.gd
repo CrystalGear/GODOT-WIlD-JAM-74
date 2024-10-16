2ae2949829f578d6990bfa22997baaabde6c1858
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2024-10-16 17:52:28 +0100

    tidied up whitespace

--- Scripts/timer_canvas.gd
+++ Scripts/timer_canvas.gd
@@ -1,8 +1,6 @@
 extends CanvasLayer
 
 @onready var timer_text: Label = $timer_text
-
-
 @export var timer: float = 120.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
