77755366b758537654acc951803d4bc2454005fa
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2024-10-18 00:25:40 +0100

    Added a function that returns a float between 0 and 1 indicating how long remains on the timer

--- Scripts/timer_canvas.gd
+++ Scripts/timer_canvas.gd
@@ -1,9 +1,18 @@
 extends CanvasLayer
 
 @onready var timer_text: Label = $timer_text
-@export var timer: float = 120.0
+@export var time_limit: float = 120.0
+var timer: float
+
+# Sets the value of timer to be equal to time_limit on start.
+func _ready() -> void:
+	timer = time_limit
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer -= delta
 	timer_text.text = str(timer)
+
+# Returns a float indicating how long remains on the timer.
+func return_time_remaining() -> float:
+	return timer / time_limit
