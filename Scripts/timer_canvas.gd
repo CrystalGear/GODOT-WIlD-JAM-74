ea7abdd4167668fa9b5cd8798a713e957ca41d20
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2024-10-18 15:14:19 +0100

    Countdown now stops when the timer hits 0.

--- Scripts/timer_canvas.gd
+++ Scripts/timer_canvas.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var timer_text: Label = $timer_text
 @export var time_limit: float = 120.0
 var timer: float
+var continueCountdown: bool = true
 
 # Sets the value of timer to be equal to time_limit on start.
 func _ready() -> void:
@@ -10,9 +11,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	timer -= delta
-	timer_text.text = str(timer)
+	if (continueCountdown):
+		timer -= delta
+		timer_text.text = str(timer)
+		
+		if (timer <= 0):
+			timer = 0
+			continueCountdown = false
 
 # Returns a float indicating how long remains on the timer.
-func return_time_remaining() -> float:
-	return timer / time_limit
+func return_normalized_time_remaining() -> float:
+	return clamp(timer / time_limit, 0 ,1)
