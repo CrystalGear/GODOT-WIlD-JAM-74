bba48ca07d5a9b2e84f627fb006fcd1b5ebcf55b
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-10-18 20:32:28 +0100

    Refactored some enemy code

--- Scripts/Enemy_Scripts/enemy.gd
+++ Scripts/Enemy_Scripts/enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+# Three states, each of which are controlled by their own respective functions.
 enum EnemyState { PATROL, CHASE, SEARCH }
 
 @export var speed: float = 3.0
@@ -17,20 +18,20 @@ var b_can_see_player: bool = false
 
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var nav_region: NavigationRegion3D = get_node("../NavigationRegion3D")
+@onready var raycast: RayCast3D = $VisionRaycast
 
 func _ready():
 	# Initializes the enemy and sets the player reference.
 	player = get_tree().root.get_node("BlockoutLevel/Player")
 	if player:
-		call_deferred("patrol")
+		call_deferred("patrol") # Required because it errors if done before the navmesh is fully mapped at runtime
 
 func handle_debug_mode():
 	# Handles state transitions and updates behavior based on the current state.
-	# debug_mode: disables detection of player and increases speed to allow observation of random pathing.
+	# debug_mode: disables detection of player and increases speed to allow observation of random pathing. Leaving in because this is the scene that will likely be tweaked most while designing over the weekend
 	if debug_mode:
 		patrol_speed = 10
 		speed = patrol_speed
-		print(transform.origin)
 		match state:
 			EnemyState.PATROL:
 				patrol_behavior()
@@ -42,7 +43,6 @@ func handle_debug_mode():
 				chase_behavior()
 			EnemyState.SEARCH:
 				search_behavior()
-		#check_for_player()
 		if b_can_see_player:
 			state = EnemyState.CHASE
 		elif state == EnemyState.CHASE and not b_can_see_player:
@@ -50,10 +50,13 @@ func handle_debug_mode():
 		elif state == EnemyState.SEARCH and not b_can_see_player:
 			state = EnemyState.PATROL
 
-func _process(delta):
-	_check_collisions()
-	handle_debug_mode()
+func rotate_enemy():
 	look_at(global_transform.origin - velocity)
+
+func _process(delta):
+	handle_debug_mode()
+	_check_collisions()
+	rotate_enemy()
 
 func patrol():
 	# Sets a random patrol point for the enemy to move towards.
@@ -66,15 +69,6 @@ func chase():
 	# Switches the enemy state to CHASE.
 	state = EnemyState.CHASE
 	speed = chase_speed
-
-func check_for_player():
-	# Checks if the player is within detection radius and transitions state accordingly.
-	if player and global_transform.origin.distance_to(player.global_transform.origin) <= detection_radius:
-		chase()
-	else:
-		if state == EnemyState.CHASE and global_transform.origin.distance_to(player.global_transform.origin) > detection_radius:
-			#patrol()
-			state = EnemyState.SEARCH
 
 func patrol_behavior():
 	# Manages the patrol behavior of the enemy.
@@ -98,8 +92,7 @@ func search_behavior():
 		patrol()
 	else:
 		move_towards_target()
-
-
+		
 func set_target_to_player():
 	# Sets the target position of the navigation agent to the player's position.
 	nav_agent.target_position = player.global_transform.origin
@@ -125,21 +118,11 @@ func get_random_patrol_point() -> Vector3:
 	return nearest_point
 
 func _check_collisions():
-	var overlaps = $VisionArea.get_overlapping_bodies()
-	if overlaps.size() > 0:
-		for overlap in overlaps:
-			if overlap.name == "Player":
-				var playerPosition = overlap.global_transform.origin
-				$VisionRaycast.force_raycast_update()
-
-				if $VisionRaycast.is_colliding():
-					var collider = $VisionRaycast.get_collider()
-
-					if collider.name == "Player":
-						$VisionRaycast.debug_shape_custom_color = Color(174,0,0)
-						print ("I SEE YOU")
-						b_can_see_player = true
-					else:
-						$VisionRaycast.debug_shape_custom_color = Color(0, 255,0)
-						print ("I DONT SEE YOU")
-						b_can_see_player = false
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if collider.name == "Player": # leaving the following debug colours in as we test over the weekend, debug ray only visible if you set Debug -> Visible Collision Shapes setting to true
+			raycast.debug_shape_custom_color = Color(174,0,0)
+			b_can_see_player = true
+		else:
+			raycast.debug_shape_custom_color = Color(0, 255,0)
+			b_can_see_player = false
