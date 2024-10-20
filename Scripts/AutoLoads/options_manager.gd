7e25ee88b993113c39ca99614e5bec52bd171c15
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-10-20 09:45:24 +0100

    updated options and audio settings

--- Scripts/AutoLoads/options_manager.gd
+++ Scripts/AutoLoads/options_manager.gd
@@ -8,7 +8,7 @@ const MAX_MOUSE_SENSE: float = 3.0
 # These setters and getters force the values to use the same validation that calling these externally uses
 @export_range(MIN_MOUSE_SENSE, MAX_MOUSE_SENSE) var mouse_sensitivity: float = 1
 @export_range(0,1) var master_volume: float = 1.0
-@export_range(0,1) var music_volume: float = 0.0
+@export_range(0,1) var music_volume: float = 1.0
 @export_range(0,1) var sfx_volume: float = 1.0
 @export_range(0,1) var ambient_volume: float = 1.0
 
